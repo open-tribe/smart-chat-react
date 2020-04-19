bddf503354e64b8026bee4040fe7a9519bc7f583
think-in-universe <mstr.raphael@gmail.com>
2020-04-20 01:55:39 +0800

    update chat APIs

--- src/chat.js
+++ src/chat.js
@@ -1,5 +1,15 @@
-import Box from '3box';
+const Box = require('3box');
 
-export const getChat = async (appName, channelName, organizer) => {
-  return await Box.getThread(appName, channelName, organizer)
+const getChat = async (appName, channelName, organizer) => {
+  const box = await Box.create()
+  return await box.openThread(appName, channelName, { firstModerator: organizer, members: true })
+}
+
+const getPosts = async (appName, channelName, organizer) => {
+  return await Box.getThread(appName, channelName, organizer, true)
+}
+
+module.exports = {
+  getChat,
+  getPosts
 }
